a8ef869ad44b515a15818f5de74361b744665ce7
Eric de Quartel <edequartel@protonmail.com>
2022-06-16 07:04:17 +0200

    Setting up the structure of the APP. Sound play stop with subsonic added

---
+++ Dots/Resources/methods.swift
@@ -0,0 +1,33 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let countries = try? newJSONDecoder().decode(Countries.self, from: jsonData)
+
+import Foundation
+
+struct Countries: Codable {
+    var country: [Country]
+}
+
+struct Country: Codable {
+    var id: Int
+    var language, code, comments, information: String
+    var method: [Method]
+    var success, failure: [String]
+}
+
+struct Method: Codable {
+    var id: Int
+    var name, developer, information, letters: String
+    var comments: String
+    var lesson: [Lesson]
+}
+
+struct Lesson: Codable {
+    var id: Int
+    var name, letters, antiletters, words: String
+    var antiwords: String
+    var sentence: [String]
+    var comments, information: String
+    var voice, audio: [String]
+}
