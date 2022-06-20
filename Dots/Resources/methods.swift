88c18965fc290831cd4ddba92e155a2c7d1f15f1
Eric de Quartel <edequartel@protonmail.com>
2022-06-20 18:00:55 +0200

    Added a label

--- Dots/Resources/methods.swift
+++ Dots/Resources/methods.swift
@@ -1,3 +1,5 @@
+// MODEL
+//
 // This file was generated from JSON Schema using quicktype, do not modify it directly.
 // To parse the JSON, add this file to your project and do:
 //
@@ -5,10 +7,12 @@
 
 import Foundation
 
+// MARK: - Countries
 struct Countries: Codable {
     var country: [Country]
 }
 
+// MARK: - Country
 struct Country: Codable {
     var id: Int
     var language, code, comments, information: String
@@ -16,6 +20,7 @@ struct Country: Codable {
     var success, failure: [String]
 }
 
+// MARK: - Method
 struct Method: Codable {
     var id: Int
     var name, developer, information, letters: String
@@ -23,6 +28,7 @@ struct Method: Codable {
     var lesson: [Lesson]
 }
 
+// MARK: - Lesson
 struct Lesson: Codable {
     var id: Int
     var name, letters, antiletters, words: String
@@ -31,3 +37,9 @@ struct Lesson: Codable {
     var comments, information: String
     var voice, audio: [String]
 }
+
+//extension Bundle {
+//    func decode<T : Decodable> (file: String)->T {
+//        guard = let url
+//    }
+//}
