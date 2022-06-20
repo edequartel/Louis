59ccfa1190d8300fffc2f9adfc69eed28ea49ec8
Eric de Quartel <edequartel@protonmail.com>
2022-06-20 18:11:04 +0200

    Adding extension to jsonmanager here called methods.swift

--- Dots/Resources/methods.swift
+++ Dots/Resources/methods.swift
@@ -38,8 +38,23 @@ struct Lesson: Codable {
     var voice, audio: [String]
 }
 
-//extension Bundle {
-//    func decode<T : Decodable> (file: String)->T {
-//        guard = let url
-//    }
-//}
+//// Extension to decode JSON locally
+extension Bundle {
+    func decode<T: Decodable>(file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Could not find \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Could not load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loadedData = try? decoder.decode(T.self, from: data) else {
+            fatalError("Could not decode \(file) from bundle.")
+        }
+        
+        return loadedData
+    }
+}
