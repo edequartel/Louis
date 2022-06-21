91a23b1793e8abe99b0c2eab803cd6118a99c828
Eric de Quartel <edequartel@protonmail.com>
2022-06-21 15:01:32 +0200

    Pickers zitten er nu in, selectie gaat ook goed nu naar acties kijken die de variabelen in de instellingen gebruiken.

--- Dots/ContentView.swift
+++ Dots/ContentView.swift
@@ -13,52 +13,33 @@ struct ContentView: View {
     
     
     var body: some View {
-        NavigationView {
-            List {
-                Text("Country")
-                ForEach(countries, id: \.id) {
-                    country in Text("\(country.language)")
+                TabView {
+                    SettingsView()
+                        .tabItem {
+                            Image(systemName: "person")
+                            Text("Instellingen")
+                        }
+                    InformationView()
+                        .tabItem {
+                            Image(systemName: "text.bubble")
+                            Text("Informatie")
+                        }
+                    PlaygroundView()
+                        .tabItem {
+                            Image(systemName: "circle.square")
+                            Text("Speelveld")
+                        }
+                    SearchWordView()
+                        .tabItem {
+                            Image(systemName: "circle.square")
+                            Text("Search Word")
+                        }
+                    AudioView()
+                        .tabItem {
+                            Image(systemName: "play")
+                            Text("Audio")
+                        }
                 }
-                Text("Methods")
-                Spacer()
-                ForEach(countries[0].method, id: \.id) {
-                    method in Text("\(method.name)")
-                }
-                Text("Lessons")
-                Spacer()
-                ForEach(countries[0].method[0].lesson, id: \.id) {
-                    lesson in Text("\(lesson.name)")
-                }
-                
-            }
-        }
-        //        TabView {
-        //            SettingsView()
-        //                .tabItem {
-        //                    Image(systemName: "person")
-        //                    Text("Instellingen")
-        //                }
-        //            InformationView()
-        //                .tabItem {
-        //                    Image(systemName: "text.bubble")
-        //                    Text("Informatie")
-        //                }
-        //            PlaygroundView()
-        //                .tabItem {
-        //                    Image(systemName: "circle.square")
-        //                    Text("Speelveld")
-        //                }
-        //            SearchWordView()
-        //                .tabItem {
-        //                    Image(systemName: "circle.square")
-        //                    Text("Search Word")
-        //                }
-        //            AudioView()
-        //                .tabItem {
-        //                    Image(systemName: "play")
-        //                    Text("Audio")
-        //                }
-        //        }
     }
 }
 
--- Dots/SettingsView.swift
+++ Dots/SettingsView.swift
@@ -9,9 +9,99 @@ import SwiftUI
 import Subsonic
 
 struct SettingsView: View {
-
+    private var countries: [Country] = Country.allCountries
+    
+    @State var selectedCountry: Int = UserDefaults.standard.integer(forKey: "CountryIndex")
+    @State var selectedMethod: Int = UserDefaults.standard.integer(forKey: "MethodIndex")
+    @State var selectedLesson: Int = UserDefaults.standard.integer(forKey: "LessonIndex")
+    
+    @State private var lettersCnt=1
+    @State private var wordCnt=3
+    
+    let action = ["mengsel","luister-schrijf","lees-schrijf","lees-luister-schrijf"]
+    @State private var actionIdx=0
+    
+    @State private var isToggle : Bool = false
+    
+    
     var body: some View {
-       Text("setting")
+        NavigationView {
+            Form {
+                List {
+                    Picker("Country", selection: $selectedCountry) {
+                        ForEach(countries, id: \.id) { country in
+                            Text(country.language)
+                        }
+                    }
+                    .onChange(of: selectedCountry) { _ in
+                        UserDefaults.standard.set(self.selectedCountry, forKey: "CountryIndex")
+                        selectedMethod = 0
+                        UserDefaults.standard.set(self.selectedMethod, forKey: "MethodIndex")
+                        selectedLesson = 0
+                        UserDefaults.standard.set(self.selectedLesson, forKey: "LessonIndex")
+                    }
+                    
+                    
+                    Picker("Methode \(selectedMethod)", selection: $selectedMethod) {
+                        ForEach(countries[selectedCountry].method, id: \.id) { method in
+                            Text(method.name)
+                        }
+                    }
+                    .onChange(of: selectedMethod) { _ in
+                        UserDefaults.standard.set(self.selectedMethod, forKey: "MethodIndex")
+                        selectedLesson=0
+                        UserDefaults.standard.set(self.selectedLesson, forKey: "LessonIndex")
+                    }
+                    
+                    Picker("Lesson \(selectedLesson)", selection: $selectedLesson) {
+                        ForEach(countries[selectedCountry].method[selectedMethod].lesson, id: \.id) { lesson in
+                            Text(lesson.name)
+                                .font(.custom(
+                                    "bartimeus6dots",
+                                    fixedSize: 32))
+//                            Text(lesson.name)
+//                                .font(.custom(
+//                                    "bartimeus6dots",
+//                                    fixedSize: 32))
+                        }
+                    }
+                    .onChange(of: selectedLesson) { _ in
+                        UserDefaults.standard.set(self.selectedLesson, forKey: "LessonIndex")
+                    }
+                    
+                    Toggle(isOn: $isToggle){
+                        Text("Auto increment")
+                    }
+                    
+                    Section(header: Text("Moeilijkheid")) {
+                        Button("Letters \(lettersCnt)") {
+                            lettersCnt += 1
+                            if lettersCnt > 10 {
+                                lettersCnt = 1
+                            }
+                        }
+                        Button("Woorden \(wordCnt)") {
+                            wordCnt += 1
+                            if  wordCnt > 10 {
+                                wordCnt = 1
+                            }
+                            
+                        }
+                    }
+                    
+                    
+                    Section(header: Text("Oefening")) {
+                        Button("Oefening \(actionIdx+1) \(action[actionIdx])") {
+                            actionIdx += 1
+                            if  actionIdx >= action.count {
+                                actionIdx = 0
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -21,3 +111,17 @@ struct SettingsView_Previews: PreviewProvider {
     }
 }
 
+//Text("Country")
+//ForEach(countries, id: \.id) {
+//    country in Text("\(country.language)")
+//}
+//Text("Methods")
+//Spacer()
+//ForEach(countries[selectedCountry].method, id: \.id) {
+//    method in Text("\(method.name)")
+//}
+//Text("Lessons")
+//Spacer()
+//ForEach(countries[selectedCountry].method[0].lesson, id: \.id) {
+//    lesson in Text("\(lesson.name)")
+//}
