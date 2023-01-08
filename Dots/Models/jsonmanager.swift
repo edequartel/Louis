13b874d3eb343ea5ebf24e20728f746cf229b1d1
Eric de Quartel <edequartel@protonmail.com>
2023-01-08 19:56:01 +0100

    refactoring
    play = practice
    getting models where they need to be

--- Dots/Models/jsonmanager.swift
+++ Dots/Models/jsonmanager.swift
@@ -7,7 +7,40 @@
 //  Use a model generator like: https://app.quicktype.io/
 
 import Foundation
+import SwiftUI
 
+class Network: ObservableObject {
+    @EnvironmentObject var network: Network
+    @Published var Languages: [Language] = []
+    
+    func getData() {
+        guard let url = URL(string: "https://www.eduvip.nl/braillestudio-software/methodslouis_edit.json") else { fatalError("Missing URL") }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                print("Request error: ", error)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else { return }
+            
+            if response.statusCode == 200 {
+                guard let data = data else { return }
+                DispatchQueue.main.async {
+                    do {
+                        let decodedLanguages = try JSONDecoder().decode([Language].self, from: data)
+                        self.Languages = decodedLanguages
+                    } catch let error {
+                        print("Error decoding: ", error)
+                    }
+                }
+            }
+        }
+        dataTask.resume()
+    }
+}
 
 struct Language: Codable, Identifiable {
     let id: Int
@@ -38,22 +71,22 @@ struct Lesson: Codable, Identifiable {
 //
 //www.eduvip.nl/braillestudio-software/methodslouis.json has to be loaded asynchronous
 // Extension to decode JSON locally
-//extension Bundle {
-//    func decode<T: Decodable>(file: String) -> T {
-//        guard let url = self.url(forResource: file, withExtension: nil) else {
-//            fatalError("Could not find \(file) in bundle.")
-//        }
-//
-//        guard let data = try? Data(contentsOf: url) else {
-//            fatalError("Could not load \(file) from bundle.")
-//        }
-//
-//        let decoder = JSONDecoder()
-//
-//        guard let loadedData = try? decoder.decode(T.self, from: data) else {
-//            fatalError("Could not decode \(file) from bundle.")
-//        }
-//
-//        return loadedData
-//    }
-//}
+extension Bundle {
+    func decode<T: Decodable>(file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Could not find \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Could not load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let loadedData = try? decoder.decode(T.self, from: data) else {
+            fatalError("Could not decode \(file) from bundle.")
+        }
+
+        return loadedData
+    }
+}
--- Dots/Views/PlaygroundView.swift
+++ Dots/Views/PlaygroundView.swift
@@ -8,59 +8,7 @@
 import SwiftUI
 import Soundable
 import SwiftProgress
-
 import AVFoundation
-
-//!use download manager
-class Network: ObservableObject {
-    @EnvironmentObject var network: Network
-    @Published var Languages: [Language] = []
-    
-    func getData() {
-        guard let url = URL(string: "https://www.eduvip.nl/braillestudio-software/methodslouis_edit.json") else { fatalError("Missing URL") }
-        
-        let urlRequest = URLRequest(url: url)
-        
-        let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            if let error = error {
-                print("Request error: ", error)
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse else { return }
-            
-            if response.statusCode == 200 {
-                guard let data = data else { return }
-                DispatchQueue.main.async {
-                    do {
-                        let decodedLanguages = try JSONDecoder().decode([Language].self, from: data)
-                        self.Languages = decodedLanguages
-                    } catch let error {
-                        print("Error decoding: ", error)
-                    }
-                }
-            }
-        }
-        dataTask.resume()
-    }
-}
-
-struct bartStijl: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-        //  .background(Color("bartimeus"))
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-
-extension View {
-    func bartStyle() -> some View {
-        modifier(bartStijl())
-    }
-}
 
 struct PlaygroundView: View {
     @EnvironmentObject var network: Network
@@ -183,7 +131,6 @@ struct PlaygroundView: View {
                     let prevSyllableString = (indexPronounce == child) ? previousItem.replacingOccurrences(of: "-", with: " ") : addSpaces(value: stripString(value: previousItem))
                     let prevtempString1 = (syllable) ? prevSyllableString :  stripString(value: previousItem)
                     
-                    
                     let  tempString = (isPlaying) && (!doubleTap) && (readSound == "after") ? prevtempString1 : tempString1
                     
                     if (indexFont==0) {
@@ -198,7 +145,6 @@ struct PlaygroundView: View {
                     }
                 }
                 
-                //====
                 TextField("", text:$input)
                     .font(.custom(monospacedFont, size: 32))
                     .foregroundColor(.blue)
@@ -267,12 +213,12 @@ struct PlaygroundView: View {
             print("\(item)")
         }
         .onAppear() {
-            network.getData() //this is asynchronous only in splashScreen at startup
-            //
-            indexLanguage=0
-            indexActivity=0
-            indexMethod=0
-            indexLesson=0
+//            network.getData() //this is asynchronous only in splashScreen at startup
+//            //
+//            indexLanguage=0
+//            indexActivity=0
+//            indexMethod=0
+//            indexLesson=0
             
             //
             if (atStartup || updateViewData) {
