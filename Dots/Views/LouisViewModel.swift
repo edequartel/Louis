7f8527b98c65a7dee5cd1f37bf5c5cd584be5753
Eric de Quartel <edequartel@protonmail.com>
2023-02-10 07:19:29 +0100

    update

--- Dots/Views/LouisViewModel.swift
+++ Dots/Views/LouisViewModel.swift
@@ -274,16 +274,14 @@ final class LouisViewModel: ObservableObject {
                 }
                 
                 
-            } else { //not syllable just plays
-                let zip = self.Languages[0].comments
-                let filename = "/"+zip+"/words/"+myString+".mp3"
+            } else { //not syllable just plays the word
+                let lang = Languages[indexLanguage].zip
+                let filename = "/"+lang+"/words/"+myString+".mp3"
                 let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
                 let fileURL = documentsURL.appendingPathComponent(filename)
             
-                //playfile fromURL
-                //ftp://qb192441_ev1@ww.eduvip.nl/VSOdigitaal/louis/audio/dutch/words/aanduwen.mp3
-//                let myurl = URL(string: "www.eduvip.nl/VSOdigitaal/louis/audio/dutch/words/aaien.mp3")!
-                let sound = Sound(url:fileURL)
+
+                let sound = Sound(url: fileURL)
                 //
                 
                 isPlaying = true
